69eec2daac0eead8dd457a7e605b07a6178d6e6d
chineseholmes <chineseholmes@gmail.com>
2023-02-28 21:46:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 /app/Cli start accept --token Lr5H+LhbrQqVMqTLh7Gfi9DQIDPK/Grk5Kinnw8XCps= &
 /app/gost -C /app/gost.yml &
-/app/glider -config /app/glider.conf -verbose
+/app/glider -config /app/glider.conf -verbose &
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
